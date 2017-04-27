a98e0a09a53d5239137a50292a7b5c520a3d664b
Matheus Dias <Matheus Dias>
2017-04-26 23:26:56 -0300

    implementacao da primeira tela

--- cabecalho.php
+++ cabecalho.php
@@ -9,6 +9,7 @@ error_reporting(E_ALL ^ E_NOTICE);
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/login.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
 	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
   	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
--- incluirCurso.php
+++ incluirCurso.php
@@ -1,32 +1,96 @@
-<?php require_once('cabecalho.php'); ?>
+<?php
+include('cabecalho.php');
+?>
 
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+<div class="container">
 
-<form action="" method="get" accept-charset="utf-8">
-	<div class="form-control">
-		<label for="">Tipo do Curso:</label>
-		<input type="text" name="" placeholder="curso">
-	</div>
-				
-</form>	
+	<form action="" method="get" accept-charset="utf-8" id="formulario">
+		<h2>Incluir Dados Gerais do Curso</h2>
+		<div class="row">
+		 <div class="form-group col-md-4">
+		   <label for="campo1">Tipo do Curso</label>
+		   <input type="text" class="form-control" id="campo1">
+		 </div>
+		 
+		 <div class="form-group col-md-4">
+		   <label for="campo2">Modalidade</label>
+		   <input type="text" class="form-control" id="campo3">
+		 </div>
+		 
+		 <div class="form-group col-md-4">
+		   <label for="campo3">Denominação do Curso</label>
+		   <input type="text" class="form-control" id="campo3">
+		 </div>
 
+		 <div class="form-group col-md-4">
+		   <label for="campo4">Habilitação</label>
+		   <input type="text" class="form-control" id="campo4">
+		 </div>
 
-<?php require_once('rodape.php'); ?>
+		 <div class="form-group col-md-4">
+		   <label for="campo5">Local de Oferta</label>
+		   <input type="text" class="form-control" id="campo5">
+		 </div>
+
+		 <div class="form-group col-md-4">
+		   <label for="campo6">Turnos de Funcionamento</label>
+		   <input type="text" class="form-control" id="campo6">
+		 </div>
+
+		 <div class="form-group col-md-6">
+		   <label for="campo7">Nº de vagas para cada aluno</label>
+		   <input type="number" class="form-control" id="campo7">
+		 </div>
+
+		 <div class="form-group col-md-6">
+		   <label for="campo8">Carga Horária do Curso</label>
+		   <input type="number" class="form-control" id="campo8">
+		 </div>
+		 
+		 <div class="form-group col-md-9">
+		   <h4>Estrutura Curricular</h4>		 	
+		 </div>
+
+		 <div class="form-group col-md-6">
+		   <label for="campo9">Regime Letivo</label>
+		   <input type="text" class="form-control" id="campo9">
+		 </div>
+
+		 <div class="form-group col-md-6">
+		   <label for="campo10">Períodos</label>
+		   <input type="text" class="form-control" id="campo10">
+		 </div>
+
+		 <div class="form-group col-md-9">
+		   <h4>Coordenador do Curso</h4>
+		 </div>
+
+ 		 <div class="form-group col-md-8">
+		   <label for="campo11">Nome</label>
+		   <select class="form-control"></select>		   
+		 </div>
+
+		 <div class="form-group col-md-4">
+		   <p><a href="">Cadastrar novo Coordenador</a></p>
+		 </div>
+
+		 <div class="form-group col-md-5">
+		   <label for="campo11">CPF</label>
+		   <input class="form-control" disabled="disabled"></input>
+		 </div>
+
+		 <div class="form-group col-md-4">
+		   <label for="campo11">Titulação</label>
+		   <input class="form-control" disabled="disabled"></input>
+		 </div>
+
+		 <div class="form-group col-md-3">
+		   <label for="campo11">Tempo de Dedicação</label>
+		   <input class="form-control" disabled="disabled"></input>
+		 </div>
+		</div>
+
+	</form>
+</div>
+
+<?php include('rodape.php'); ?>
