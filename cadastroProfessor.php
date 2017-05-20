f7a9769c9ebaa057804cda526162ef1f7b23d069
Matheus Dias <Matheus Dias>
2017-05-20 16:06:48 -0300

    mais telas

--- cadastroProfessor.php
+++ cadastroProfessor.php
@@ -50,26 +50,65 @@
 	    </div>
 
 	    <div id="atuacaoIES" class="tab-pane fade">
-	      <h3></h3>
-	      <div class="container">
+	       
+	      <div class="container" style="margin-top: 60px;">
 			<ul class="nav nav-pills nav-stacked col-md-2">
 			  <li class="active"><a href="#geral" data-toggle="pill">Geral</a></li>
 			  <li><a href="#atuacaoProfissional" data-toggle="pill">Atuação Profissional</a></li>
 			  <li><a href="#publicacoes" data-toggle="pill">Publicações</a></li>
 			</ul>
 			<div class="tab-content col-md-10">
-			        
 			        <div class="tab-pane fade in active" id="geral">
-
 			        	<form action="" method="get" accept-charset="utf-8">
-			        				
-				        	<div class="row">
-				        		<label for="">Matrícula</label>
-				        		<input class="form-control" type="text" id="datepicker"></input>
-				        		
+					    <div class="row">
+				        	<div class="col-md-4">
+				        		<label for="">Matrícula *</label>
+				        		<input class="form-control col-md-4" type="text" required="true"></input>
 				        	</div>
-						</form>
+				        	<div class="col-md-4"> 
+				        		<label for="">Data Admissão *</label>
+				        		<input class="form-control col-md-8" type="text" id="datepicker" required="true"></input>	
+				        	</div>
+						</div>
+			        	
+			        	<div class="row" style="text-align: center">
+				        	<hr>
+				        	<label for="">Informe a quantidade de horas das atividades</label>
+				        </div>
 
+				        	<div class="row">				        		
+					        	<div class="col-md-2">
+						        	<input class="form-control" type="number"></input><label for="">Horas NDE</label>
+					        	</div>
+				        	</div>
+
+				        	<div class="row">
+					        	<div class="col-md-2">
+						        	<input class="form-control" type="number"></input><label for="">Orientação TCC</label>       		
+					        	</div>
+				        	</div>
+
+				        	<div class="row">
+					        	<div class="col-md-2">
+						        	<input class="form-control" type="number"></input><label for="">Coordenação Curso</label>
+					        	</div>				        		
+				        	</div>
+
+				        	<div class="row">
+					        	<div class="col-md-2">
+						        	<input class="form-control" type="number"></input>
+						        	<label for="">Coordenação Outros Cursos</label>					        	
+					        	</div>				        		
+				        	</div>
+
+				        	<div class="row">
+					        	<div class="col-md-2">
+						        	<input class="form-control" type="number"></input>
+						        	<label for="">Pesquisa (semestre atual)</label>					        	
+					        	</div>				        		
+				        	</div>
+				        
+						</form>
 			        </div>
 
 			        <div class="tab-pane fade" id="atuacaoProfissional">
