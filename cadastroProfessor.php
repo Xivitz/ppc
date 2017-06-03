4af03f96a83157542a27a4ac13f19aa3a00eea20
Matheus Dias <Matheus Dias>
2017-06-03 18:45:57 -0300

    criando classe curso

--- cadastroProfessor.php
+++ cadastroProfessor.php
@@ -1,7 +1,7 @@
 <?php
 require_once('cabecalho.php');
 require_once('logicaUsuario.php');
-//verificaUsuario();
+verificaUsuario();
  ?>
 
 
---
+++ class/Curso.php
@@ -0,0 +1,28 @@
+<?php 
+
+	/**
+	* 
+	*/
+	class Curso
+	{
+		
+		public $id;
+		public $tipoCurso;
+		public $modalidade;
+		public $denominacaoCurso;
+		public $habilitacao;
+		public $localOferta;
+		public $turno;
+		public $vagas;
+		public $cargaHoraria;
+
+
+		function __construct(argument)
+		{
+			# code...
+		}
+	}
+
+
+
+ ?>
